0f722fce32ff52c1ff35bdaaadcc43fcbedd7ba5
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-12-09 20:06:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,9 @@ def won?(board)
   return false if board == [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
   WIN_COMBINATIONS.each do |item|
-    next if item.map { |index| board[index] == ' ' }
+    spots = item.map { |index| board[index] == ' ' }
+
+    next if
 
     return item if board[item[0]] == board[item[1]] && board[item[1]] == board[item[2]]
   end
