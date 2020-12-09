b0d23cbf792fc1accb45d5c0a410835618ff5477
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-12-09 20:41:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,12 +18,12 @@ def won?(board)
   return false if board == [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
   WIN_COMBINATIONS.each do|item|
-    spots = [board[item[0]], board[item[1]], board[item[2]]] # spots ornek: ['O', ' ', 'O']
+    spots = [board[item[0]], board[item[1]], board[item[2]]] # spots ornek: ['X', ' ', 'O']
 
     next if board.include?(' ')
 
     spots.all? do |spot|
-      spot == spots[0]
+      spot == spots[]
     end
   end
 
