1d7e1ea06517984b6596d0201c915f059c82ac0a
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-12-09 21:23:39 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -46,9 +46,9 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) || draw?(board) || full?(board) 
+  if won?(board) || draw?(board) || full?(board)
     true
-  else 
+  else
     false
   end
 end
